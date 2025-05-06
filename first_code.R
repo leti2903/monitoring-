890483418216775f1eea236d2fd54695d063846d
leti2903 <letizia.bregante17@gmail.com>
2025-05-06 17:02:04 +0200

    Create first_code.R

---
+++ first_code.R
@@ -0,0 +1,11 @@
+install.packages("terra")
+library(terra) #package used for remote sensing data 
+
+#Importing data 
+setwd("C:\\utente/Desktop/") #used backslah not slah 
+
+rast("Matogrosso l5 1992219 lrg.jpg")
+mato 
+banda 1 vediamo infrarosso e si trova dopo il visibile 
+banda 2 il rosso 
+banda 3 in verde 
